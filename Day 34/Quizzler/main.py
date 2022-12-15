6bfddd33d0487febbab4c009e4a2c5d263bcd875
p0c4 <73047020+p0c4@users.noreply.github.com>
2022-12-15 15:30:39 -0800

    Update main.py

--- Day 34/Quizzler/main.py
+++ Day 34/Quizzler/main.py
@@ -17,5 +17,3 @@ quiz_interface = ui.QuizInterface(quiz)
 while quiz.still_has_questions():
     quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
