e254a2aeb91f64b12cf64ee29a32e4eb4e119b9d
p0c4 <73047020+p0c4@users.noreply.github.com>
2023-01-10 12:36:16 -0800

    Add Day 60 Files and Blog Project Contact Form

--- Day 59/Blog Capstone Project Part 2/main.py
+++ Day 59/Blog Capstone Project Part 2/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 
 blog_response = requests.get("https://api.npoint.io/5140b693af8574dc2ead").json()
@@ -15,9 +16,26 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html",is_successful=False)
+    elif request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        my_email = "xxxxxxxxxx@gmail.com"
+        my_password = "xxxxxxxxxx"
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=my_email,
+                msg=f"Subject:New Form\n\nName:{name}\nEmail:{email}\nPhone:{phone}\nMessage:{message}"
+            )
+        return render_template("contact.html", is_successful=True)
 
 
 @app.route("/post/<num>")
